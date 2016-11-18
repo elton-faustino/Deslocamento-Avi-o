a6375b136c3f1205f244b0cba78043362e6abebd
elton-faustino <elton-faustino@hotmail.com>
2016-11-18 20:17:02 -0200

    FullProject
    FullProject

---
+++ airplane/airplane/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace airplane
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblCoseno = new System.Windows.Forms.Label();
+            this.lblSeno = new System.Windows.Forms.Label();
+            this.lblAngulo = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Angulo:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.label2.Location = new System.Drawing.Point(12, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Seno:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.label3.Location = new System.Drawing.Point(12, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Coseno:";
+            // 
+            // lblCoseno
+            // 
+            this.lblCoseno.AutoSize = true;
+            this.lblCoseno.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.lblCoseno.Location = new System.Drawing.Point(69, 54);
+            this.lblCoseno.Name = "lblCoseno";
+            this.lblCoseno.Size = new System.Drawing.Size(0, 13);
+            this.lblCoseno.TabIndex = 5;
+            // 
+            // lblSeno
+            // 
+            this.lblSeno.AutoSize = true;
+            this.lblSeno.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.lblSeno.Location = new System.Drawing.Point(69, 32);
+            this.lblSeno.Name = "lblSeno";
+            this.lblSeno.Size = new System.Drawing.Size(0, 13);
+            this.lblSeno.TabIndex = 4;
+            // 
+            // lblAngulo
+            // 
+            this.lblAngulo.AutoSize = true;
+            this.lblAngulo.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.lblAngulo.Location = new System.Drawing.Point(69, 9);
+            this.lblAngulo.Name = "lblAngulo";
+            this.lblAngulo.Size = new System.Drawing.Size(0, 13);
+            this.lblAngulo.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::airplane.Properties.Resources.airplane_icon;
+            this.pictureBox1.Location = new System.Drawing.Point(61, 150);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblCoseno);
+            this.Controls.Add(this.lblSeno);
+            this.Controls.Add(this.lblAngulo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblCoseno;
+        private System.Windows.Forms.Label lblSeno;
+        private System.Windows.Forms.Label lblAngulo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ airplane/airplane/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace airplane
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Bitmap _bitmap = new Bitmap(Properties.Resources.airplane_icon);
+        private float _angulo = 0;
+        private Timer _timer = new Timer();
+        private PointF _pontoSC = new Point(Screen.PrimaryScreen.WorkingArea.Width / 2, 550);
+        private const float _deslocamento = 1f;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = false;
+            _timer.Interval = 10;
+            _timer.Tick += (ProcessarEventosTimer);
+            _timer.Start();
+        }
+
+        private void ProcessarEventosTimer(object sender, EventArgs e)
+        {
+            var novoPonto = ObterSenoConseno(_angulo);
+
+            _pontoSC.X = _pontoSC.X + novoPonto.X;
+            _pontoSC.Y = _pontoSC.Y + novoPonto.Y;
+
+            Refresh();
+        }
+
+        private Bitmap RotateImage(Bitmap bmp, float angle)
+        {
+            var rotatedImage = new Bitmap(bmp.Width, bmp.Height);
+
+            using (var g = Graphics.FromImage(rotatedImage))
+            {
+                g.TranslateTransform(bmp.Width / 2, bmp.Height / 2);
+
+                g.RotateTransform(angle);
+
+                g.TranslateTransform(-bmp.Width / 2, -bmp.Height / 2);
+
+                g.DrawImage(bmp, new PointF(0, 0));
+            }
+
+            return rotatedImage;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            RotacionarAviao(e, ref _angulo);
+        }
+
+        private void RotacionarAviao(KeyEventArgs e, ref float angulo)
+        {
+
+            ValidarAngulo(ref angulo);
+
+            ObterSenoConseno(angulo);
+
+            if (e.KeyCode == Keys.Right)
+                angulo += _deslocamento;
+
+            if (e.KeyCode == Keys.Left)
+                angulo -= _deslocamento;
+
+            Refresh();
+        }
+
+        private PointF ObterSenoConseno(float angulo)
+        {
+            var seno = Math.Sin((Math.PI / 180) * angulo);
+            var coseno = Math.Cos((Math.PI / 180) * angulo);
+
+            lblAngulo.Text = angulo.ToString();
+            lblSeno.Text = seno.ToString();
+            lblCoseno.Text = coseno.ToString();
+
+            return new PointF((float)coseno, (float)seno);
+        }
+
+        private void ValidarAngulo(ref float angulo)
+        {
+            if (angulo < 0)
+                angulo = 360;
+
+            if (angulo > 360)
+                angulo = 0;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            var g = e.Graphics;
+
+            g.DrawImage(RotateImage(_bitmap, _angulo)
+                , _pontoSC);
+
+            g.DrawLine(new Pen(Color.Red), new Point(Screen.PrimaryScreen.WorkingArea.Width / 2,
+                Screen.PrimaryScreen.WorkingArea.Height / 2),  new PointF(_pontoSC.X + 90, _pontoSC.Y + 95));
+
+            g.DrawEllipse(new Pen(Color.Black), Screen.PrimaryScreen.WorkingArea.Width / 2 - 50,
+                Screen.PrimaryScreen.WorkingArea.Height / 2 - 50, 100, 100);
+        }
+    }
+}
